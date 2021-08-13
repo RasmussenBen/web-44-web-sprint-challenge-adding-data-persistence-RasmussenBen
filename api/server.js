f22234f91a6cdc4f992cf6fcaebf1467baabb0d4
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-13 13:15:23 -0500

    index and server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,26 @@
-// build your server here and require it from index.js
+const express = require('express')
+const helmet = require('helmet')
+const projectsRouter = require('./project/router')
+const resourcesRouter = require('./resource/router')
+const tasksRouter = require('./task/router')
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
+
+server.use('*', (req, res, next) => {
+    next({status: 404,
+        message: 'Page not found'
+    })
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server
